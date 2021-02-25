47aecf5a751759a9d79348079eaab80cdbff295a
Dhruvil-Dave <67459224+Dhruvil-Dave@users.noreply.github.com>
2021-02-24 16:11:55 -0800

    Updates

---
+++ precache-manifest.393369f194ced8813b3b75d1ad27c909.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ca9b8fd0f27a62dc75ed18e4e570275",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da3065c028066bad6173",
+    "url": "/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "8e612e905ac356759f44",
+    "url": "/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "da3065c028066bad6173",
+    "url": "/static/js/2.5b8251f3.chunk.js"
+  },
+  {
+    "revision": "a8b062ea113d5347eaa037e7d1931e01",
+    "url": "/static/js/2.5b8251f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e612e905ac356759f44",
+    "url": "/static/js/main.e29ee3e0.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
